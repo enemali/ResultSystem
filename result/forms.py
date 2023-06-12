7a47a337d0c51bafb289ceab26563c026ecebfff
enemali <malitician@gmail.com>
2023-06-12 19:01:04 +0100

    commit

--- result/forms.py
+++ result/forms.py
@@ -94,7 +94,6 @@ class ImageForm(forms.ModelForm):
 class settingForm(forms.ModelForm):
     class Meta:
         model = setting
-
         fields = 'current_Term','current_Session','date_Term_Begin','date_Term_End','number_of_days_school_open','next_term_begins'
         # fields = ['announcement','announcement_date','news']
         widgets = {'announcement_date' : DatePickerInput(), 'next_term_begins' : DatePickerInput(), 'date_Term_Begin' : DatePickerInput(), 'date_Term_End' : DatePickerInput()}
